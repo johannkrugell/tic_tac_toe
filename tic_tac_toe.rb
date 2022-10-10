195ba28259de1281d6d6be54e198776333ff4370
johannkrugell <johannkrugell@gmail.com>
2022-10-11 05:41:42 +1100

    INITIALIZE REPOSITORY Initialize repository for Tic Tac Toe program
    On branch main
    
    Initial commit
    
    Changes to be committed:
    new file:   tic_tac_toe.rb

---
+++ tic_tac_toe.rb
@@ -0,0 +1,12 @@
+# Welcome to Tic Tac Toe
+# Are you ready Player1? Please enter your details
+# Create Player1 Object
+# Create Player2 Object
+# Create an Empty Board Object
+# Loop until all selections on the board is made or a player win
+# Player1 makes selection
+# Display updated board
+# Check for winner
+# Player2 makers select
+# Display updated board
+# Check for winner
