77944738d47550d4d927ca33a076d3b629518fda
johannkrugell <johannkrugell@gmail.com>
2022-10-12 06:29:40 +1100

    PLAYER CLASS Created a player class that initializes with name and marker. Added code that prompt the user for their name and marker, stores the object in a hash with the key player1 or player2. Loops through the creation of the hashes using an each loop for two players
    On branch player-class
    Changes to be committed:
    new file:   lib/player.rb
    modified:   tic_tac_toe.rb

---
+++ lib/player.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# class creating the player object
+class Player
+  attr_accessor :name, :marker
+
+  def initialize(name, marker)
+    @name = name
+    @marker = marker
+  end
+
+  def set_marker(new_marker)
+    @marker = new_marker
+  end
+end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,24 @@
+# frozen_string_literal: true
+
+require './lib/player'
+
 # Welcome to Tic Tac Toe
+puts "Let's play Tic Tac Toe!"
+
 # Are you ready Player1? Please enter your details
 # Create Player1 Object
-# Create Player2 Object
+# To do: can the player creation be written as a loop?
+players = {}
+i = ['1', '2']
+i.each do
+  puts "Player #{i} please enter your name"
+  name = gets.chomp
+  puts "Player #{i} please select your marker"
+  marker = gets.chomp
+  players[:"player#{i}"] = Player.new(name, marker)
+  # p players[:player1].instance_variable_get(:@marker)
+end
+
 # Create an Empty Board Object
 # Loop until all selections on the board is made or a player win
 # Player1 makes selection
